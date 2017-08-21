c166636091cae91ee58cef8ccfa8136df51ae12e
mrjkc <julius.kvedaras@coupa.com>
2017-08-21 21:13:40 +0100

    first assignment

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 (function () {
-'user strict';
-angular.module('AngularApp', [])
+  'user strict';
+  angular.module('AngularApp', [])
 
-.controller('AngularController', function() {
+  .controller('control', control);
 
-  });
+  control.$inject = ['$scope'];
+
+  function control($scope) {
+    $scope.output = "";
+    $scope.showMessage = function() {
+      arr = $scope.name;
+      if (arr !== undefined) {
+        arr = $scope.name.split(",");
+        console.log(arr.length);
+        if (arr.length <= 3) {
+          $scope.output = "Enjoy!";
+        } else if (arr.length > 3){
+          $scope.output = "This is too much!";
+        }
+      } else {
+        $scope.output = "Please enter data!";
+      }
+    };
+  }
 })();
